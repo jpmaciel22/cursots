1724c909e397540174652955e114977260f5684b
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2024-12-18 11:11:14 -0300

    TYPESCRIPT TYPESCRIPTTYPESCRIPTTYPESCRIPTTYPESCRIPTTYPESCRIPTTYPESCRIPTT TYPESCRIPTTYPESCRIPTTYPESCRIPTTYPESCRIPTTYPESCRIPTTYPESCRIPTTYPESCRIPTT TYPESCRIPTTYPESCRIPTTYPESCRIPTTYPESCRIPTTYPESCRIPTTYPESCRIPTTYPESCRIPTTYPESCRIPT

--- src/aulas/typeAssertions.ts
+++ src/aulas/typeAssertions.ts
@@ -1,5 +1,8 @@
 const body1 = document.querySelector('body');
 if(body1) body1.style.backgroundColor = 'red';
 
-const body2 = document.querySelector('body')!;
+const body2 = document.querySelector('body')!; // non-null assertion
   body2.style.backgroundColor = 'red';
+
+const body3 = document.querySelector('body') as HTMLBodyElement; // type assertion
+body3.style.backgroundColor = 'red';
