e1dccb856d663ae283c2943fa3c2335fbc42c2d5
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-01-30 14:14:37 -0300

    abstract TypeScript

--- src/aulas/abstract.ts
+++ src/aulas/abstract.ts
@@ -1,4 +1,4 @@
-export class Personagem{
+export abstract class Personagem{
   constructor(
     protected nome: string,
     protected ataque: number,
@@ -7,18 +7,30 @@ export class Personagem{
 
   atacar(personagem: Personagem): void{
     console.log(`${this.nome} está atacando ${personagem.nome}.`)
+    this.bordao()
     personagem.perderVida(this.ataque);
     console.log(`${personagem.nome} está com ${personagem.vida} de vida restantes.`)
   }
   perderVida(ataque: number): number{
     return this.vida = this.vida - ataque
   }
+  abstract bordao():void; // é necessário que haja uma chamada do método na classe abstrata
+  // porém o mesmo não pode ter 'corpo', isso cabe às classes que extendem a classe abstrata personagem
+  // para decidir.
 }
 
 
-class Cavaleiro extends Personagem {}
-class Elfa extends Personagem {}
+class Mago extends Personagem {
+  bordao():void{
+    console.log('Soul Repentance. 🪄')
+  }
+}
+class Elfa extends Personagem {
+  bordao():void{
+    console.log('Não perderei dessa vez...')
+  }
+}
 
-const Joao = new Cavaleiro('Joao',130,99);
+const Joao = new Mago('Joao',130,99);
 const Maria = new Elfa('Maria',1000,25);
 Joao.atacar(Maria);
