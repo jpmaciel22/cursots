70f2f05e02472395226a4feaf6b29f87d9bcc92d
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-02-03 15:09:13 -0300

    VIDEOPLAYER COMPLETO E FUNCIONANDO.

--- src/interfaces-classes-video/index.ts
+++ src/interfaces-classes-video/index.ts
@@ -10,20 +10,45 @@ interface VideoPlayerProtocol {
   executarPrograma(): void;
 }
 
-export default class VideoPlayer implements VideoPlayerProtocol, VideoPlayerElements{
+export default class VideoPlayer implements VideoPlayerProtocol{
   private videoPlayer: HTMLVideoElement;
   private playButton: HTMLButtonElement;
   private stopButton: HTMLButtonElement;
 
-  constructor(){}
+  constructor(videoPlayerElements: VideoPlayerElements){
+    this.videoPlayer = videoPlayerElements.videoPlayer;
+    this.playButton = videoPlayerElements.playButton;
+    this.stopButton = videoPlayerElements.stopButton;
+  }
 
   playToggle(): void{
-
+    if(this.videoPlayer.paused){
+      this.videoPlayer.play();
+      this.playButton.innerText = 'Pause';
+    }else{
+      this.videoPlayer.pause();
+      this.playButton.innerText = 'Play'
+    }
   }
   stop(): void{
-
+  //
   }
   executarPrograma(): void{
-
+  this.playButton.addEventListener('click', () => {
+    this.playToggle();
+  });
+  this.stopButton.addEventListener('click',() => {
+    this.videoPlayer.pause();
+    this.videoPlayer.currentTime = 0;
+    this.playButton.innerText = 'Play'
+  })
   }
 }
+
+const videoPlayer = new VideoPlayer({
+  videoPlayer: document.querySelector('.video') as HTMLVideoElement,
+  playButton:document.querySelector('.play') as HTMLButtonElement,
+  stopButton: document.querySelector('.stop') as HTMLButtonElement
+})
+
+videoPlayer.executarPrograma();
