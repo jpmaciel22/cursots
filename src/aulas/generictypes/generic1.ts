fc6fe70761b170b9f8c0da92ec67d7628fccd5e0
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-02-04 15:55:09 -0300

    generic types são muito inteligentes

--- src/aulas/generictypes/generic1.ts
+++ src/aulas/generictypes/generic1.ts
@@ -1,12 +1,12 @@
-type FilterCallback = (
-  value: unknown,
-  array?: unknown[],
+type FilterCallback<TipoGenerico> = (
+  value: TipoGenerico,
+  array?: TipoGenerico[],
   index?: number,
 ) => boolean;
 
 export function MyFilterFunction<TipoGenerico>(
   array: TipoGenerico[],
-  callbackfn: FilterCallback
+  callbackfn: FilterCallback<TipoGenerico>
 ): TipoGenerico[]{
   const novoArray = [];
 
@@ -24,7 +24,14 @@ const array = [1,2,3,4,5,6,7,8,9,10];
 const filteredArrayDefault = array.filter((value) => value < 5)
 console.log(filteredArrayDefault)
 const myFilteredArray = MyFilterFunction(array,(value) => {
-  if(typeof value === 'number')return value < 5;
-  return false
+  // if(typeof value === 'number'){
+  return value < 5; //} // agora nao precisa mais dessas adaptações pois o typescript ja entendeu que é um number
+  // return false
 });
 console.log(myFilteredArray)
+
+
+// Ao colocarmos <TipoGenerico> podemos permitir à função de receber o tipo que será esperado para a função,
+// portanto nao precisamos colocar unknown e adaptar a funcao ao tipo que iremos receber,
+//  mas sim colocar TipoGenerico e o typescript de forma inteligente saberá qual tipo receberá a partir
+// da variavel chamando esta funcao.
