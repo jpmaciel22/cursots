88741d3573f4627f04be1fbd29fd67fd2a449024
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-02-04 15:41:33 -0300

    generic criando a nossa funcao filter

---
+++ src/aulas/generictypes/generic1.ts
@@ -0,0 +1,28 @@
+type FilterCallback = (
+  value: unknown,
+  array?: unknown[],
+  index?: number,
+) => boolean;
+
+function MyFilterFunction(array: unknown[], callbackfn: FilterCallback): unknown[]{
+
+  const novoArray = [];
+
+  for(let i = 0;i<array.length;i++){
+    if(callbackfn(array[i])){
+      novoArray.push(array[i]);
+    }
+  }
+
+  return novoArray;
+}
+
+const array = [1,2,3,4,5,6,7,8,9,10];
+
+const filteredArrayDefault = array.filter((value) => value < 5)
+console.log(filteredArrayDefault)
+const myFilteredArray = MyFilterFunction(array,(value) => {
+  if(typeof value === 'number')return value < 5;
+  return false
+});
+console.log(myFilteredArray)
