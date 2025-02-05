d38979c9b17182640cb4373ab5d5f78f50586dcf
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-02-05 15:30:25 -0300

    predicacao de tipos

---
+++ src/aulas/generictypes/generic7-typePredicate.ts
@@ -0,0 +1,19 @@
+export function isNumber(value: unknown): value is number /**boolean */ {
+  return typeof value === 'number'
+};
+
+export function soma<G>(...args: G[]): number | null{
+  const retorno = args.reduce((sum,value)=>{
+    if(isNumber(value) && isNumber(sum)){
+      return sum + value;
+    }
+    return sum;
+  },0)
+  return retorno;
+}
+// aqui no caso nao estava dando certo antes, quando na primeira funcao estava dando como retorno
+//  'boolean', pois aparentemente o typescript não consegue passar a inferência de 'value é number' para fora
+//  do escopo daquela função, então mesmo fazendo typeguard na segunda função, ainda assim o typescript
+// reclamava de soma de NUMBER COM G.
+//  portanto precisamos afirmar na primeira funcao q o retorno DAQUELE BOOLEAN é que O VALOR É UM NUMERO
+// entao ele precisa desse contexto a mais sobre o que aqle true retornado significa.
