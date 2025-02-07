01532919163f0ca817ed55643b26ddce4910926d
jpmaciel22 <jpbmaciel@gmail.com>
2025-02-06 23:57:41 -0300

    CONVERSOR DE MOEDA FINALIZADO

--- src/projeto-moeda/index.ts
+++ src/projeto-moeda/index.ts
@@ -34,25 +34,16 @@ export default class Aplicativo implements Metodos {
        const valorInserido = this.valorInserido.value
        const de = this.from.value
        const para = this.to.value
-    //    async function fetchData() { // pra pegar os dados da api do json
-    //     const url = 'https://economia.awesomeapi.com.br/json/last/'+de+'-'+para
-    //     const response = await fetch(url);
-    //     const dadosObtidos = await response.json();
-    //     const valorHigh:string = dadosObtidos[de+para].high;
-    //     this.resultado.innerText = (Number(valorHigh) * Number(valorInserido)).toString()
-    this.fetchData(de, para, valorInserido);
-    };
-    private async fetchData(de: string, para: string, valorInserido: string) {
-        const url = 'https://economia.awesomeapi.com.br/json/last/'+ de+'-'+para;
+       const resultadoEscopo = this.resultado
+       async function fetchData() { // pra pegar os dados da api do json
+        const url = 'https://economia.awesomeapi.com.br/json/last/'+de+'-'+para
         const response = await fetch(url);
         const dadosObtidos = await response.json();
-        const valorHigh: string = dadosObtidos[de+para]?.high;
-        if (valorHigh) {
-            const resultadoFinal = (Number(valorHigh) * Number(valorInserido)).toFixed(2);
-            this.resultado.innerText = `${resultadoFinal} ${para}`;
-        } else {
-            this.resultado.innerText = 'Erro ao obter os dados.';
-        }
+        const valorHigh:string = dadosObtidos[de+para].high;
+        const resultadoFinal = (Number(valorHigh) * Number(valorInserido)).toFixed(2)
+        resultadoEscopo.innerText = `${resultadoFinal} ${para}`
+       }
+       fetchData();
     }
 }
 
